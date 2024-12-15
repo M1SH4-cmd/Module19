b96549c79457272d743d9cf33e2440ef30451ab5
M1SH4-cmd <6el0zu61979@mail.ru>
2024-12-15 08:37:51 +0700

    Task3 done

---
+++ HW/Task3/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int maxPayment = 0;
+    string monthEmployee;
+
+    ifstream employeesList;
+    employeesList.open("list.txt");
+
+    vector<string> buffer;
+
+    while(!employeesList.eof()){
+        string employee;
+        getline(employeesList, employee, '\n');
+        buffer.push_back(employee);
+    }
+
+    for(int i = 0; i < buffer.size(); ++i){
+        istringstream iss(buffer[i]);
+        string name;
+        string scndName;
+        double payment;
+        iss >> name >> scndName >> payment;
+        sum += payment;
+        if(payment > maxPayment){
+            maxPayment = payment;
+            monthEmployee = name + " " + scndName;
+        }
+    }
+
+
+    cout << "Sum of all payments:\t" << sum << endl;
+    cout << "Biggest payment worker:\t" << monthEmployee << endl;
+
+    employeesList.close();
+    return 0;
+}
